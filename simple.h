5201fe0520d4168619fbe5de13b03afd207e517a
Sankar சங்கர் <sankar.curiosity@gmail.com>
2013-04-19 15:42:34 +0530

    Use fixed sized POSIX datatypes instead of platform dependent types

--- simple.h
+++ simple.h
@@ -2,10 +2,10 @@ const int SIMPLEFS_MAGIC = 0x10032013;
 const int SIMPLEFS_DEFAULT_BLOCK_SIZE = 4 * 1024;
 
 struct simplefs_super_block {
-	unsigned int version;
-	unsigned int magic;
-	unsigned int block_size;
-	unsigned int free_blocks;
+	uint32_t version;
+	uint32_t magic;
+	uint32_t block_size;
+	uint32_t free_blocks;
 
-	char padding[(4 * 1024) - (4 * sizeof(unsigned int))];
+	char padding[(4 * 1024) - (4 * sizeof(uint32_t))];
 };
