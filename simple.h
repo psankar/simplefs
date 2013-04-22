f2ae6c1536ea17d4344ac4a9b896916d8b3ba99d
Sankar சங்கர் <sankar.curiosity@gmail.com>
2013-04-22 17:02:29 +0530

    Code comments

--- simple.h
+++ simple.h
@@ -2,9 +2,21 @@
 #define SIMPLEFS_DEFAULT_BLOCK_SIZE 4096
 #define SIMPLEFS_FILENAME_MAXLEN 255
 
+/* Hard-coded inode number for the root directory */
 const int SIMPLEFS_ROOT_INODE_NUMBER = 1;
+
+/* The disk block where super block is stored */
+const int SIMPLEFS_SUPERBLOCK_BLOCK_NUMBER = 0;
+
+/* The disk block where the inodes are stored */
+const int SIMPLEFS_INODESTORE_BLOCK_NUMBER = 1;
+
+/* The disk block where the name+inode_number pairs of the
+ * contents of the root directory are stored */
 const int SIMPLEFS_ROOTDIR_DATABLOCK_NUMBER = 2;
 
+/* The name+inode_number pair for each file in a directory.
+ * This gets stored as the data for a directory */
 struct simplefs_dir_record {
 	char filename[SIMPLEFS_FILENAME_MAXLEN];
 	uint32_t inode_no;
@@ -31,9 +43,3 @@ struct simplefs_super_block {
 
 	char padding[SIMPLEFS_DEFAULT_BLOCK_SIZE - (4 * sizeof(uint32_t)) - sizeof(struct simplefs_inode)];
 };
-
-/*
-struct simplefs_dir_contents {
-	uint32_t children_count;
-	struct simplefs_dir_record records[];
-}; */
